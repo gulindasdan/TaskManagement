30e6405f696e0718c85d9a58f5a280f51ba5c7ed
özkan <ozkan.kocc2@gmail.com>
2020-09-29 23:38:18 +0300

    Removed empty lines

--- TaskManagement/TaskManagement.Core/DataAccess/IRepository.cs
+++ TaskManagement/TaskManagement.Core/DataAccess/IRepository.cs
@@ -9,7 +9,6 @@ namespace TaskManagement.Core.DataAccess
 {
     public interface IRepository<TEntity, TKey> where TEntity : class, IEntity<TKey>, new()
     {
-
         Task<IEnumerable<TEntity>> GetAllAsync();
         Task<TEntity> GetByIdAsync(TKey key);
         Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity,bool>> predicate);
@@ -20,6 +19,5 @@ namespace TaskManagement.Core.DataAccess
         Task Update(TEntity entity);
         Task Remove(TEntity entity);
         Task RemoveRange(IEnumerable<TEntity> entities);
-
     }
 }
