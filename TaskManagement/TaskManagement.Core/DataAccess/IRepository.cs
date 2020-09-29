47936831c5929d69dc68e84ed62932f09034f144
Gülin Daşdan <gulindasdann@gmail.com>
2020-09-29 23:20:07 +0300

    created core tier

---
+++ TaskManagement/TaskManagement.Core/DataAccess/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TaskManagement.Core.Entities;
+
+namespace TaskManagement.Core.DataAccess
+{
+    public interface IRepository<TEntity, TKey> where TEntity : class, IEntity<TKey>, new()
+    {
+
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(TKey key);
+        Task<IEnumerable<TEntity>> Find(Expression<Func<TEntity,bool>> predicate);
+        Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity,bool>> predicate);
+        TEntity Get(TEntity entity);
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+        Task Update(TEntity entity);
+        Task Remove(TEntity entity);
+        Task RemoveRange(IEnumerable<TEntity> entities);
+
+    }
+}
---
+++ TaskManagement/TaskManagement.Core/Entities/IEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskManagement.Core.Entities
+{
+    public interface IEntity : IEntity<int> { }
+    public interface IEntity<TKey>
+    {
+        TKey Id { get; set; }
+        DateTime? CreatedDate { get; set; }
+
+        DateTime? ModifiedDate { get; set; }
+        bool? isActive { get; set; }
+        
+    }
+}
+
