13859d763cfb7eff515be0079e9c4c5f9fd075f4
Gülin Daşdan <gulindasdann@gmail.com>
2020-11-15 20:21:51 +0300

    account manager added

---
+++ TaskManagement/TaskManagement.API/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManagement.API.Models.Request;
+using TaskManagement.Business.Abstract;
+using TaskManagement.Entities;
+
+namespace TaskManagement.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+        private readonly IMapper _mapper;
+
+        public AccountController(IAccountService accountService, IMapper mapper)
+        {
+            _accountService = accountService;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginModel)
+        {
+            var user = _accountService.Login(loginModel.Email, loginModel.Password);
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return Unauthorized();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] RegisterRequest registerModel)
+        {
+            var user = await _accountService.FindByEmailAsync(registerModel.Email);
+            if(user == null)
+            {
+                user = _mapper.Map<AppUser>(registerModel);
+                await _accountService.Register(user);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
--- TaskManagement/TaskManagement.Entities/AppUser.cs
+++ TaskManagement/TaskManagement.Entities/AppUser.cs
@@ -7,5 +7,7 @@ namespace TaskManagement.Entities
 {
     public class AppUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
